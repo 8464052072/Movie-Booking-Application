26b6fca8b75e52b518b6f8256efa00d0b748e03a
8464052072 <gopichanduarepalli@gmail.com>
2024-09-08 18:51:16 +0530

    second commit

--- movie-service/src/main/java/com/example/MovieServiceApplication.java
+++ movie-service/src/main/java/com/example/MovieServiceApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
 //@EntityScan(basePackages = {"com.example.repository","com.example.model","com.example.Config"}) // --> used when all the classes are not part of the subpackages of the main class package 
 public class MovieServiceApplication {
 
